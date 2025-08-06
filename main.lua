7766e24ef6b84eff0d202a9ea689f00040fccf6b
arsoc29424 <bizarro22424@gmail.com>
2025-08-06 16:58:53 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -80,7 +80,7 @@ local AvailableKeys = {
     {Name = "X", Key = Enum.KeyCode.X},
     {Name = "Y", Key = Enum.KeyCode.Y},
     {Name = "Z", Key = Enum.KeyCode.Z},
-    
+
     -- Teclas especiais
     {Name = "SHIFT", Key = Enum.KeyCode.LeftShift},
     {Name = "CTRL", Key = Enum.KeyCode.LeftControl},
@@ -88,13 +88,14 @@ local AvailableKeys = {
     {Name = "TAB", Key = Enum.KeyCode.Tab},
     {Name = "CAPS", Key = Enum.KeyCode.CapsLock},
     {Name = "SPACE", Key = Enum.KeyCode.Space},
-    
+
     -- Mouse
     {Name = "MOUSE1", Key = Enum.UserInputType.MouseButton1},
     {Name = "MOUSE2", Key = Enum.UserInputType.MouseButton2},
     {Name = "MOUSE3", Key = Enum.UserInputType.MouseButton3},
-    {Name = "MOUSE5", Key = Enum.UserInputType.MouseButton5}
-    
+    {Name = "MOUSE4", Key = Enum.UserInputType.MouseButton3},
+    {Name = "MOUSE5", Key = Enum.UserInputType.MouseButton5}, -- <-- vírgula adicionada aqui
+
     -- F Keys
     {Name = "F1", Key = Enum.KeyCode.F1},
     {Name = "F2", Key = Enum.KeyCode.F2},
@@ -109,6 +110,7 @@ local AvailableKeys = {
     {Name = "F11", Key = Enum.KeyCode.F11},
     {Name = "F12", Key = Enum.KeyCode.F12}
 }
+
 
 -- ESTADO DO SISTEMA
 local SystemState = {
